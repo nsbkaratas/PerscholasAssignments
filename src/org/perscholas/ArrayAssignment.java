535b6c3dd3e9d41141976f0cadd6c3de86501392
Nesibe Karatas <nsbkaratas@gmail.com>
2022-12-01 16:41:50 -0500

    array assignment fixed

--- src/org/perscholas/ArrayAssignment.java
+++ src/org/perscholas/ArrayAssignment.java
@@ -6,17 +6,17 @@ import java.util.Scanner;
 public class ArrayAssignment {
 
 	public static void main(String[] args) {
-//		question1();
-//		question2();
-//		question3();
-//		question4();
-//		question5();
-//		question6();
-//		question7();
-//		question8();
-//		question9();
-//		question10();
-//		question11();
+		question1();
+		question2();
+		question3();
+		question4();
+		question5();
+		question6();
+		question7();
+		question8();
+		question9();
+		question10();
+		question11();
 		question12();
 	}
 	public static void question1() {
